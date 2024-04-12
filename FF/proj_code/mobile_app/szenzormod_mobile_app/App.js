c5528903881c1e627828454dcd028e83a80600e0
Balázs Tóth <grandmastar27@gmail.com>
2024-04-12 20:18:24 +0200

    Code updated

--- FF/proj_code/mobile_app/szenzormod_mobile_app/App.js
+++ FF/proj_code/mobile_app/szenzormod_mobile_app/App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text>Sensormodality project</Text>
+		</SafeAreaView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#1F1F1F",
+	},
+	title: {
+		fontSize: 25,
+		fontWeight: "bold",
+		textAlign: "left",
+	},
 });
--- FF/proj_code/proj_code.ino
+++ FF/proj_code/proj_code.ino
@@ -114,6 +114,7 @@ void setupEEPROM() {
   if (testWifi())
   {
     Serial.println("Succesfully Connected!!!");
+    launchWeb(); // TODO: Add web socket OR MQTT connection for live data exchange!
     return;
   }
   else
@@ -277,72 +278,83 @@ void setupAP(void)
 
 void createWebServer()
 {
- {
-    server.on("/", []() {
+  Serial.println("Web server started.");
 
-      IPAddress ip = WiFi.softAPIP();
-      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
-      content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
-      content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
-      content += ipStr;
-      content += "<p>";
-      content += st;
-      content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
-      content += "</html>";
-      server.send(200, "text/html", content);
-    });
-    server.on("/scan", []() {
-      //setupAP();
-      IPAddress ip = WiFi.softAPIP();
-      String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+  server.on("/", []() {
 
-      content = "<!DOCTYPE HTML>\r\n<html>go back";
-      server.send(200, "text/html", content);
-    });
+    IPAddress ip = WiFi.softAPIP();
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+    content = "<!DOCTYPE HTML>\r\n<html>Hello from ESP8266 at ";
+    content += "<form action=\"/scan\" method=\"POST\"><input type=\"submit\" value=\"scan\"></form>";
+    content += ipStr;
+    content += "<p>";
+    content += st;
+    content += "</p><form method='get' action='setting'><label>SSID: </label><input name='ssid' length=32><input name='pass' length=64><input type='submit'></form>";
+    content += "</html>";
+    server.send(200, "text/html", content);
+  });
+  server.on("/scan", []() {
+    //setupAP();
+    IPAddress ip = WiFi.softAPIP();
+    String ipStr = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
 
-    server.on("/setting", []() {
-      String qsid = server.arg("ssid");
-      String qpass = server.arg("pass");
-      if (qsid.length() > 0 && qpass.length() > 0) {
-        Serial.println("clearing eeprom");
-        for (int i = 0; i < 96; ++i) {
-          EEPROM.write(i, 0);
-        }
-        Serial.println(qsid);
-        Serial.println("");
-        Serial.println(qpass);
-        Serial.println("");
+    content = "<!DOCTYPE HTML>\r\n<html>go back";
+    server.send(200, "text/html", content);
+  });
 
-        Serial.println("writing eeprom ssid:");
-        for (int i = 0; i < qsid.length(); ++i)
-        {
-          EEPROM.write(i, qsid[i]);
-          Serial.print("Wrote: ");
-          Serial.println(qsid[i]);
-        }
-        Serial.println("writing eeprom pass:");
-        for (int i = 0; i < qpass.length(); ++i)
-        {
-          EEPROM.write(32 + i, qpass[i]);
-          Serial.print("Wrote: ");
-          Serial.println(qpass[i]);
-        }
-        EEPROM.commit();
-
-        content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
-        statusCode = 200;
-        ESP.reset();
+  server.on("/temperature", []() {
+      float humidity = dht.readHumidity();
+      float temperature = dht.readTemperature();
+      if (isnan(humidity) || isnan(temperature)) {
+          server.send(500, "text/plain", "Failed to read from DHT sensor!");
       } else {
-        content = "{\"Error\":\"404 not found\"}";
-        statusCode = 404;
-        Serial.println("Sending 404");
+          String json = "{\"humidity\": " + String(humidity, 1) + ", \"temperature\": " + String(temperature, 1) + "}";
+          server.send(200, "application/json", json);
       }
-      server.sendHeader("Access-Control-Allow-Origin", "*");
-      server.send(statusCode, "application/json", content);
+  });
 
-    });
-  } 
-}
+  server.on("/setting", []() {
+    String qsid = server.arg("ssid");
+    String qpass = server.arg("pass");
+    if (qsid.length() > 0 && qpass.length() > 0) {
+      Serial.println("clearing eeprom");
+      for (int i = 0; i < 96; ++i) {
+        EEPROM.write(i, 0);
+      }
+      Serial.println(qsid);
+      Serial.println("");
+      Serial.println(qpass);
+      Serial.println("");
+
+      Serial.println("writing eeprom ssid:");
+      for (int i = 0; i < qsid.length(); ++i)
+      {
+        EEPROM.write(i, qsid[i]);
+        Serial.print("Wrote: ");
+        Serial.println(qsid[i]);
+      }
+      Serial.println("writing eeprom pass:");
+      for (int i = 0; i < qpass.length(); ++i)
+      {
+        EEPROM.write(32 + i, qpass[i]);
+        Serial.print("Wrote: ");
+        Serial.println(qpass[i]);
+      }
+      EEPROM.commit();
+
+      content = "{\"Success\":\"saved to eeprom... reset to boot into new wifi\"}";
+      statusCode = 200;
+      ESP.reset();
+    } else {
+      content = "{\"Error\":\"404 not found\"}";
+      statusCode = 404;
+      Serial.println("Sending 404");
+    }
+    server.sendHeader("Access-Control-Allow-Origin", "*");
+    server.send(statusCode, "application/json", content);
+
+  });
+} 
 
 void readDHTSensor() {
   float humidity = dht.readHumidity();
@@ -387,6 +399,7 @@ void updateStatusBar(float humidity, float temperature, bool isConnected) {
 
 void loop() {
   // Wait a few seconds between measurements.
+  server.handleClient();
   delay(2000);
   
   // Reading temperature and humidity
